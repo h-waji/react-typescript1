2b70b5e444ba7c9906926909f1fbf246ce203119
hamru <goo.hiro.w@gmail.com>
2024-03-12 12:37:42 +0900

    optional chaining

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { Todo } from './Todo';
 import { TodoType } from './types/todo';
 import { Text } from './Text';
+import { UserProfile } from './UserProfile';
+import { User } from './types/user'
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,9 +17,15 @@ export default function App() {
     });
   };
 
+  const user: User = {
+    name: "Hamham",
+    // hobbies: ["sleeping", "zoning out", "wheel"],
+  }
+
   return (
     <div>
       <Text color='gray' fontSize='66px' />
+      <UserProfile user={user} />
       <button onClick={onClickFetchData}>FetchData</button>
       {todos.map((todo) => (
         <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
